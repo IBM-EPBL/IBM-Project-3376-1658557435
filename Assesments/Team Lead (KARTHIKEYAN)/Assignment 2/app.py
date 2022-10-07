ccd520b0a0e3a726f7544b656a9340c1d4f3b0b8
karthiTox <karthibalu2001@gmail.com>
2022-10-07 20:49:34 +0530

    updating assignment 2 as per the new instruction

--- Assesments/Team Lead (KARTHIKEYAN)/Assignment 2/app.py
+++ Assesments/Team Lead (KARTHIKEYAN)/Assignment 2/app.py
@@ -1,10 +1,65 @@
+from asyncio.windows_events import NULL
 from flask import Flask, request, render_template
+import ibm_db
+
+# This code will also execute locally so it is not mandatory for you to provide ibm db2 credentials
+
+conn = NULL
+
+try:
+  # 👇 replace with your IBM DB2 Credentials before executing
+  conn = ibm_db.connect("DATABASE=<databasename>;HOSTNAME=<your-hostname>;PORT=<portnumber>;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=<username>;PWD=<password>",'','')
+except:
+  print("[APP] fallback to local db so please set IBM DB2 credentials")
 
 app = Flask(__name__)
 
-users = {
-  
-}
+def init_db2(): 
+    if conn != NULL:
+      create_user_table_sql = "CREATE TABLE IF NOT EXISTS Users (userId int NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), firstname varchar(255), lastname varchar(255), email varchar(255), password varchar(255), PRIMARY KEY(userId))"
+      ibm_db.exec_immediate(conn, create_user_table_sql)
+      print(f"[DB2] successfully exected create table sql command\n[DB2] {create_user_table_sql}")
+
+
+init_db2()
+
+def insert_user_db2(firstname, lastname, email, password):
+    if(not fetch_user_db2(email=email)):
+      insert_sql = "INSERT INTO Users(firstname, lastname, email, password) VALUES (?, ?, ?, ?)"
+      prep_stmt = ibm_db.prepare(conn, insert_sql)
+      ibm_db.bind_param(prep_stmt, 1, firstname)
+      ibm_db.bind_param(prep_stmt, 2, lastname)
+      ibm_db.bind_param(prep_stmt, 3, email)
+      ibm_db.bind_param(prep_stmt, 4, password)
+      ibm_db.execute(prep_stmt)
+      print(f"[DB2] inserted user - {email}")
+    else:
+      print(f"[DB2] already present with {email}")
+
+def fetch_user_db2(email):    
+    insert_sql = "SELECT * FROM Users WHERE email = ?"
+    prep_stmt = ibm_db.prepare(conn, insert_sql)
+    ibm_db.bind_param(prep_stmt, 1, email)
+    ibm_db.execute(prep_stmt)
+    account = ibm_db.fetch_assoc(prep_stmt)
+    print(f"[DB2] fetched user - {account}")
+    return account
+
+users = {}
+
+def insert_user_local(firstname, lastname, email, password):    
+    users[email] = {
+      "FIRSTNAME": firstname,
+      "LASTNAME": lastname,
+      "EMAIL": email,
+      "PASSWORD": password,
+    }
+
+def fetch_user_local(email):    
+    if email in users:
+      return users[email]
+    else:
+      return NULL    
 
 @app.route("/")
 @app.route("/home")
@@ -21,12 +76,16 @@ def signin():
     email = request.form["email"]
     password = request.form["password"]        
 
+    user = NULL;
 
+    try:
+      user = fetch_user_db2(email=email)
+    except:
+      user = fetch_user_local(email=email)   
 
-    if email in users:
-      user = users[email]
-      if user["password"] == password:
-        return render_template("tq.html", fname=user["fname"], lname=user["lname"], email=user["email"])
+    if "PASSWORD" in user:
+      if user["PASSWORD"] == password:
+        return render_template("tq.html", fname=user["FIRSTNAME"], lname=user["LASTNAME"], email=user["EMAIL"])
 
     return render_template("wrong_cred.html", email=email)
     
@@ -41,13 +100,11 @@ def signup():
     email = request.form["email"]
     password = request.form["password"]        
 
-    users[email] = {
-      "fname": fname,
-      "lname": lname,
-      "email": email,
-      "password": password,
-    }
-    print(users)
+    try:
+      insert_user_db2(firstname=fname, lastname=lname, email=email, password=password)
+    except:
+      insert_user_local(firstname=fname, lastname=lname, email=email, password=password)   
+
     return render_template("tq.html", fname=fname, lname=lname, email=email)
   else:
     return render_template("signup.html")
--- Assesments/Team Lead (KARTHIKEYAN)/Assignment 2/readme.md
+++ Assesments/Team Lead (KARTHIKEYAN)/Assignment 2/readme.md
@@ -1,11 +1,30 @@
 # Assignment 2 - Flask Application
-- [x] create flask app
-- [x] Added bootstrap
-- [x] Added Home, About, Signin, Signup
-- [x] Added Signin and Signup functionality
+- [x] Create a Flask App
+- [x] Add the Home page, About Page
+- [x] Add the Bootstrap
+- [x] Add the Sign in page and add the Signup Page with database connectivity
+- [x] Use IBM Db2 as Database
 
-## for testing 
+## How to execute? 
 
-- cd current_assignment_dir
-- python -m flask run | or | flask run
+> This code will also execute locally so it is not mandatory for you to provide ibm db2 credentials so step 1 and 2 is not mandatory
+ 
+**step 1**  _(optional but needed for IBM DB2)_
+
+👇 replace with your IBM DB2 Credentials before executing
+
+``` python
+conn = ibm_db.connect("DATABASE=<databasename>;HOSTNAME=<your-hostname>;PORT=<portnumber>;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=<username>;PWD=<password>",'','')
+
+```
+
+**step 2** _(optional but needed for IBM DB2)_
+
+Download 'DigiCertGlobalRootCA.crt' file from IBM DB2 console and paste it along with 'app.py' file
+
+**step 3** _Must_
+
+- ```python -m flask run```  
+- (or)  
+- ```flask run```
 - visit localhost:5000
